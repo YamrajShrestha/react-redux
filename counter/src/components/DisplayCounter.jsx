b8309632128ee098ec4618b877a4083843c6826a
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-02-27 18:10:53 +0545

    use section is set up after creating

--- counter/src/components/DisplayCounter.jsx
+++ counter/src/components/DisplayCounter.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
-  const counter = useSelector((store) => store.counter);
+  const { counterVal } = useSelector((store) => store.counter);
   return (
     <>
-      <p className="lead mb-4">Display Current Value: <button className="btn btn-warning px-5">{counter}</button></p>
+      <p className="lead mb-4">
+        Display Current Value:{" "}
+        <button className="btn btn-warning px-5">{counterVal}</button>
+      </p>
     </>
   );
 };
--- counter/src/store/index.js
+++ counter/src/store/index.js
@@ -1,51 +1,55 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const INITIAL_VALUE = {
-  counter: 0,
-  privacy: false,
-};
+// const INITIAL_VALUE = {
+//   counter: 0,
+//   privacy: false,
+// };
 
 const counterSlice = createSlice({
-  name: 'counter',
-  initialState: {counterVal: 0},
+  name: "counter",
+  initialState: { counterVal: 0 },
   reducers: {
-    increment: (state, action)=>{
-
+    increment: (state, action) => {
+      console.log(state, action);
     },
-    decrement: (state, action)=>{
-
+    decrement: (state, action) => {
+      console.log(state, action);
     },
-    add: (state, action)=>{
-
+    add: (state, action) => {
+      console.log(state, action);
     },
-    subtract: (state, action)=>{
-
-    }
-
-  }
+    subtract: (state, action) => {
+      console.log(state, action);
+    },
+  },
 });
 
-const counterReducer = (store = INITIAL_VALUE, action) => {
-  if (action.type === "INCREMENT") {
-    return { ...store, counter: store.counter + 1 };
-  } else if (action.type === "DECREMENT") {
-    return { ...store, counter: store.counter - 1 };
-  } else if (action.type === "ADD") {
-    return {
-      ...store,
-      counter: store.counter + Number(action.payload.num),
-    };
-  } else if (action.type === "SUBTRACT") {
-    return {
-      ...store,
-      counter: store.counter - Number(action.payload.num),
-    };
-  } else if (action.type === "PRIVACY_TOGGLE") {
-    return { ...store, privacy: !store.privacy };
-  }
-  return store;
-};
+// const counterReducer = (store = INITIAL_VALUE, action) => {
+//   if (action.type === "INCREMENT") {
+//     return { ...store, counter: store.counter + 1 };
+//   } else if (action.type === "DECREMENT") {
+//     return { ...store, counter: store.counter - 1 };
+//   } else if (action.type === "ADD") {
+//     return {
+//       ...store,
+//       counter: store.counter + Number(action.payload.num),
+//     };
+//   } else if (action.type === "SUBTRACT") {
+//     return {
+//       ...store,
+//       counter: store.counter - Number(action.payload.num),
+//     };
+//   } else if (action.type === "PRIVACY_TOGGLE") {
+//     return { ...store, privacy: !store.privacy };
+//   }
+//   return store;
+// };
 
-const counterStore = configureStore({ reducer: {} });
+const counterStore = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
 
+export const counterActions = counterSlice.actions;
 export default counterStore;
