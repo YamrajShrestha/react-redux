be2f0ed86d57828e29c648883fde091e3dd06664
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-02-27 13:15:24 +0545

    added action.payload to additional buttons

--- counter/src/components/Button.jsx
+++ counter/src/components/Button.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 const Button = () => {
   const dispatch = useDispatch();
+  const inputElement = useRef();
 
   const handleIncrement = () => {
     dispatch({ type: "INCREMENT" });
@@ -11,6 +12,17 @@ const Button = () => {
   const handleDecrement = () => {
     dispatch({ type: "DECREMENT" });
   };
+
+  const handleAdd = () => {
+    dispatch({ type: "ADD", payload: { num: inputElement.current.value } });
+    inputElement.current.value = "";
+  };
+
+  const handleSubtract = () => {
+    dispatch({ type: "SUBTRACT", payload: { num: inputElement.current.value } });
+    inputElement.current.value = "";
+  };
+
   return (
     <>
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
@@ -36,11 +48,17 @@ const Button = () => {
             className="form-control"
             placeholder="Enter a number"
             aria-label="Recipient's username with two button addons"
+            ref={inputElement}
           />
-          <button className="btn btn-outline-primary" type="button">
+          <button
+            className="btn btn-outline-primary"
+            type="button"
+            onClick={handleAdd}
+          >
             Add
           </button>
-          <button className="btn btn-outline-info" type="button">
+          <button className="btn btn-outline-info" type="button"
+          onClick={handleSubtract}>
             Subtract
           </button>
         </div>
