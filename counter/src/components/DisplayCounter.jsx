a85ba2c2fed4065da63df7693de2224157dd8779
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-02-27 11:18:12 +0545

    set couter value using useSeledtor at DisplayCounter.jsx

--- counter/src/components/DisplayCounter.jsx
+++ counter/src/components/DisplayCounter.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
+  const counter = useSelector((store) => store.counter);
   return (
-    < >
-      <p className="lead mb-4">
-           Display Current Value:
-          </p>
-    </ >
-  )
-}
+    <>
+      <p className="lead mb-4">Display Current Value: {counter}</p>
+    </>
+  );
+};
 
-export default DisplayCounter
+export default DisplayCounter;
